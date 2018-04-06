230ba0202be38f88d2e9986c9c2bdbad562fd095
zulkarnay <bedelbaev@gmail.com>
2018-04-06 23:40:16 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "https://rubygems.org"
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+gem "pg", ">= 0.18", "< 2.0"
+gem "puma", "~> 3.7"
+gem "rails", "~> 5.1.6"
+
+group :development, :test do
+  gem "factory_bot_rails"
+  gem "faker"
+  gem "pry"
+  gem "rspec-rails", "~> 3.7"
+  gem "rubocop", require: false
+  gem "rubocop-rspec", require: false
+end
+
+group :development do
+  gem "listen", ">= 3.0.5", "< 3.2"
+  gem "web-console", ">= 3.3.0"
+end
---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Permissions
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs:    false,
+        request_specs: false,
+        routing_specs: false,
+        helper_specs:  false
+    end
+  end
+end
